cd8ddc41b8f32b8002638258487d552c0ac21fe5
Noora Liv <j.czinzoll@ingressy.de>
2025-12-21 18:00:20 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author ingressy
+ * @version 0.1
+ * Program that's handles school grades for one people
+ */
+
+import gui.WindowManager;
+
+public class Main {
+    /**
+     * main function
+     */
+    public static void main() {
+        WindowManager.window();
+    }
+}
